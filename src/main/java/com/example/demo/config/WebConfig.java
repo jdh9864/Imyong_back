b78a00b3c36c9759db8b4f5aae1f7046fccf3c17
donghyeck jung <jdh98649874@gmail.com>
2026-09-14 18:48:28 +0900

    프론트 배포 후 접근 허용

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 1. /api/** 대신 /** 로 설정하여 모든 경로 허용
                 .allowedOriginPatterns(
-                        "https://*.vercel.app",  // 2. Vercel 메인 및 미리보기 배포 주소
-                        "http://localhost:3000"   // 로컬 개발 환경
+                        "https://imyong-front-yi5m-lime.vercel.app/",  // 2. Vercel 메인 및 미리보기 배포 주소
+                        "http://localhost:5175"   // 로컬 개발 환경
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
